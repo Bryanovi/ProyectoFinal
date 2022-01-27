81deb298b519d34845b5f769c074baadfa6fdc94
unknown <dianajazminpf@gmail.com>
2022-01-26 20:45:19 -0500

    Base de Datos subida

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -14,7 +14,8 @@ public class ProyectoFinal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexion cn = new Conexion();
+        cn.conectar();
     }
     
 }
