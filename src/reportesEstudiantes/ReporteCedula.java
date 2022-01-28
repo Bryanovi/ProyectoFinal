f154042df42e2006320a37dbada193b1a781a836
unknown <dianajazminpf@gmail.com>
2022-01-27 19:40:54 -0500

    Reporte subido

--- src/reportesEstudiantes/ReporteCedula.java
+++ src/reportesEstudiantes/ReporteCedula.java
@@ -5,6 +5,7 @@
  */
 package reportesEstudiantes;
 
+
 import java.awt.BorderLayout;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,36 +18,38 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.view.JRViewer;
 import proyectofinal.Conexion;
 
+
 /**
  *
  * @author DELL
  */
-public class EstudiantesReporte extends javax.swing.JFrame {
+public class ReporteCedula extends javax.swing.JFrame {
 
     /**
      * Creates new form EstudiantesReporte
      */
-    public EstudiantesReporte() {
+    public ReporteCedula() {
         initComponents();
+        
     }
 
     public void cargarReporte() {
         try {
             // TODO add your handling code here:
             Conexion cc = new Conexion();
-            Map ced = new HashMap();
+            Map ape = new HashMap();
             
-            ced.put("ced",jtxtCedula.getText());
-            JasperReport reporte;
+            ape.put("ced",jtxtCedula.getText());
+            JasperReport reporte;         
             reporte = JasperCompileManager.compileReport("C:/reportesEstudiantes/reporteEstudiante.jrxml");
-            JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, ced, cc.conectar());
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, ape, cc.conectar());
             JRViewer jRViewer = new JRViewer(jasperPrint);
-            jPanelR.removeAll();
-            jPanelR.setLayout(new BorderLayout());
-            jPanelR.add(jRViewer, BorderLayout.CENTER);
+            jPanel2.removeAll();
+            jPanel2.setLayout(new BorderLayout());
+            jPanel2.add(jRViewer, BorderLayout.CENTER);
             jRViewer.setVisible(true);
-            jPanelR.repaint();
-            jPanelR.revalidate();
+            jPanel2.repaint();
+            jPanel2.revalidate();
 
         } catch (JRException ex) {
             JOptionPane.showMessageDialog(null, ex);
@@ -59,9 +62,9 @@ public class EstudiantesReporte extends javax.swing.JFrame {
         jbtnReporte = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jtxtCedula = new javax.swing.JTextField();
-        jPanelR = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jbtnReporte.setText("Reporte");
         jbtnReporte.addActionListener(new java.awt.event.ActionListener() {
@@ -72,47 +75,41 @@ public class EstudiantesReporte extends javax.swing.JFrame {
 
         jLabel1.setText("Ingrese la cédula");
 
-        javax.swing.GroupLayout jPanelRLayout = new javax.swing.GroupLayout(jPanelR);
-        jPanelR.setLayout(jPanelRLayout);
-        jPanelRLayout.setHorizontalGroup(
-            jPanelRLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 0, Short.MAX_VALUE)
         );
-        jPanelRLayout.setVerticalGroup(
-            jPanelRLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 572, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 594, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGap(75, 75, 75)
+                .addGap(90, 90, 90)
                 .addComponent(jLabel1)
-                .addGap(33, 33, 33)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jtxtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
-                        .addComponent(jbtnReporte)))
-                .addContainerGap(752, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
                 .addGap(27, 27, 27)
-                .addComponent(jPanelR, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jtxtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(jbtnReporte)
+                .addContainerGap(643, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jtxtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(jbtnReporte)
-                .addGap(39, 39, 39)
-                .addComponent(jPanelR, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jtxtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jbtnReporte))
+                .addGap(48, 48, 48)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -120,7 +117,7 @@ public class EstudiantesReporte extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbtnReporteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnReporteActionPerformed
-        cargarReporte();
+       cargarReporte();
     }//GEN-LAST:event_jbtnReporteActionPerformed
 
     /**
@@ -140,14 +137,22 @@ public class EstudiantesReporte extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EstudiantesReporte.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteCedula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EstudiantesReporte.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteCedula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EstudiantesReporte.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteCedula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EstudiantesReporte.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteCedula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -160,14 +165,14 @@ public class EstudiantesReporte extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EstudiantesReporte().setVisible(true);
+                new ReporteCedula().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanelR;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JButton jbtnReporte;
     private javax.swing.JTextField jtxtCedula;
     // End of variables declaration//GEN-END:variables
